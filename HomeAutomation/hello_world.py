000f528ee5ed96d5bf66a77f4e27a228640d51f3
Sunil Dhummi <sunil.dhummi@thetradedesk.com>
2019-12-30 22:46:43 +1100

    renamed files, build docker from docker-compose, introduced .travis.yml

--- HomeAutomation/hello_world.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-msg = "hello world"
-print(msg)
-
-today = datetime.now()
-print(today.strftime("%d/%m/%Y %H:%M:%S"))
--- Monitoring/grafana.dockerfile
+++
@@ -1,9 +0,0 @@
-FROM grafana/grafana
-USER root
-
-RUN apk update
-RUN apk add
-RUN apk add --update curl
-
-#expose port
-EXPOSE 3000
